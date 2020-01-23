b9b1ddbd780acfd1a20d4f1d444d37fcbc95da4a
Hemanth <your e-mail>
2020-01-23 13:36:03 -0500

    Add arrowfunction.js

---
+++ arrowfunctions.js
@@ -0,0 +1,11 @@
+const add = (arg1, arg2) => {
+    return arg1 + arg2;
+}
+
+const subtract = (arg1, arg2) => arg1 - arg2;
+
+const square = num => num * num;
+
+add(2, 1); // 3
+subtract(2, 1); // 1
+square(2); // 4
