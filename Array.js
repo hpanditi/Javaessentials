d82163ae2030fe119e5303bc8d0b87a336d179e6
Hemanth <your e-mail>
2020-01-23 10:59:36 -0500

    Add Array.js

---
+++ Array.js
@@ -0,0 +1,9 @@
+let foods = [];
+foods.push('pizza');
+foods.push('burgers');
+
+foods; // ['pizza', 'tacos', 'burgers'];
+foods[0]; // 'pizza'
+foods[foods.length - 1]; // burgers
+foods.pop(); // burgers
+foods; // ['pizza', 'tacos']
