780cf55d93677a826fe6c2454671dd8f0a45ae09
Hemanth <your e-mail>
2020-01-23 14:29:38 -0500

    Add Promise.js

---
+++ Promise.js
@@ -0,0 +1,18 @@
+const myLongRunningFn = () => {
+    return new Promise((resolve, reject) => {
+        set Timeout (() => {
+            if (Math.random() < 0.5) {
+                resolve('success!');
+            } else {
+                reject(new Error('failure!'));
+            }
+        }, 2000);
+    });
+};
+
+const doWork = () => {
+    myLongRunningFn()
+        .then(response => {
+            console.log(response); // failure!
+        });
+    };
