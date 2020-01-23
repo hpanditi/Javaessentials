0eda36937eb17d78ee5188e54682d27677ceef96
Hemanth <your e-mail>
2020-01-23 14:14:46 -0500

    Add Export.js

---
+++ Export.js
@@ -0,0 +1,9 @@
+// Name exports
+export const myFunction = () => 'hi!';
+const myOtherFunction = () => 'goodbye';
+
+export { myFunction, myOtherFunction };
+
+const myDefaultFunction = () => 'potato';
+
+export default myDefaultFunction;
