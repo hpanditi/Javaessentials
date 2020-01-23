9b05780650cee6fdad21bfbeb611859bc2b25041
Hemanth <your e-mail>
2020-01-23 12:12:05 -0500

    Add switch.js

---
+++ switch.js
@@ -0,0 +1,15 @@
+let color = 'Orange'; 
+
+switch (color) {
+    case 'Red': 
+        console.log('go red!');
+        break;
+    case 'Blue':
+        console.log('go blue!');
+        break;
+    case 'Orange':
+        console.log('go orange!'); 
+        break;
+    default:
+        console.log("sorry, that's not a valid color");
+}        
