a80891301a0e46096d4ad2ef44e9c37c69ff1962
Hemanth <your e-mail>
2020-01-23 11:47:58 -0500

    Add Equality.js

---
+++ Equality.js
@@ -0,0 +1,11 @@
+1 === 1; // true
+'hello' === 'hello'; // true
+
+1 ===2; // false
+'hello' === 'goodbye'; // false
+
+1 !==1; // false
+'hello' !== 'hello'; // false
+
+1 !==2; // true
+'hello' !== 'goodbye'; // true
