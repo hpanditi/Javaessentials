ece86d3ee9336e44fa613f7aae8e8e3dc1547040
Hemanth <your e-mail>
2020-01-23 14:22:38 -0500

    Add Import.js

---
+++ Import.js
@@ -0,0 +1,8 @@
+import exportExampleDefault, {
+    myFunction,
+    myOtherFunction,
+} from './exportExample.js';
+
+myFunction(); // hi!
+myOtherFunction(); // goodbye
+exportExampleDefault(); // potato
